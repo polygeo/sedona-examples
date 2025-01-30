b4b41941eb506aafdfbe09b5593de8c67202b285
Matthew Powers <matthewpowers@Matthews-MacBook-Pro-2.local>
2025-01-30 15:40:27 -0500

    add a join touches example

---
+++ tests/test_joins.py
@@ -0,0 +1,57 @@
+from pyspark.sql import SparkSession
+from sedona.spark import *
+from .sedona import sedona
+from sedona.sql.types import GeometryType
+from sedona.sql.st_predicates import ST_DWithin
+from sedona.sql.st_constructors import ST_Point, ST_GeomFromText
+from pyspark.sql.functions import col, lit
+from pyspark.sql.types import (
+    StructType,
+    StructField,
+    IntegerType,
+    StringType,
+    FloatType,
+)
+from pathlib import Path
+from sedona.stats.clustering.dbscan import dbscan
+import chispa
+
+
+def test_join_touches():
+    # sql = """
+    # SELECT GeometryType(
+    #     ST_GeomFromText('LINESTRING(2.0 4.0,4.0 0.0)')
+    # ) as result
+    # UNION 
+    # SELECT GeometryType(
+    #     ST_GeomFromText('LINESTRING(6.0 0.0,10.0 4.0)')
+    # ) 
+    # as result;
+    # """
+    df = sedona.createDataFrame([
+        ("a", 'LINESTRING(2.0 4.0,4.0 0.0)'),
+        ("b", 'LINESTRING(6.0 0.0,10.0 4.0)'),
+    ], ["id", "geometry"])
+    lines = df.withColumn("geometry", ST_GeomFromText(col("geometry")))
+    lines.show(truncate=False)
+    lines.createOrReplaceTempView("lines")
+
+    df = sedona.createDataFrame([
+        ("x", 'POLYGON((6.0 2.0,6.0 4.0, 8.0 4.0, 8.0 2.0, 6.0 2.0))'),
+    ], ["id", "geometry"])
+    polygons = df.withColumn("geometry", ST_GeomFromText(col("geometry")))
+    polygons.show(truncate=False)
+    polygons.createOrReplaceTempView("polygons")
+
+    sedona.sql("""
+    SELECT
+        lines.id as line_id,
+        polygons.id as polygon_id
+    FROM
+        lines
+    INNER JOIN
+        polygons
+    ON
+        ST_Touches(lines.geometry, polygons.geometry);        
+    """).show()
+
