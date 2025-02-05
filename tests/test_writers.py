45461f27cc740de2a65ccd8b654b8f0abac93495
Matthew Powers <matthewpowers@Mac.lan>
2025-02-05 18:23:40 -0500

    update write geojson test

--- tests/test_writers.py
+++ tests/test_writers.py
@@ -7,14 +7,14 @@ import chispa
 import pytest
 
 
-def test_geometrytype():
+def test_write_geojson():
     df = sedona.createDataFrame([
         ("a", 'LINESTRING(2.0 5.0,6.0 1.0)'),
         ("b", 'LINESTRING(7.0 4.0,9.0 2.0)'),
         ("c", 'LINESTRING(1.0 3.0,3.0 1.0)'),
-    ], ["id", "geom"])
-    actual = df.withColumn("geom", ST_GeomFromText(col("geom")))
+    ], ["id", "geometry"])
+    actual = df.withColumn("geometry", ST_GeomFromText(col("geometry")))
     print("***")
     actual.printSchema()
     actual.show()
-    actual.write.format("geoparquet").mode("overwrite").save("/tmp/a_thing")
+    actual.write.format("geojson").mode("overwrite").save("/tmp/a_thing")
