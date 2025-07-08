4fe2051fd6470baaf477f56a9440320507ecdb2d
Matthew Powers <matthewkevinpowers@gmail.com>
2025-07-07 20:25:12 -0400

    update h3 example

--- tests/test_h3.py
+++ tests/test_h3.py
@@ -14,15 +14,15 @@ def test_H3CellDistance():
 
     empire_state_building = Point(-73.985428, 40.748817)
     freedom_tower = Point(-74.013379, 40.712743)
+    willis_tower = Point(-87.635918, 41.878876)
 
     df = sedona.createDataFrame([
         (empire_state_building, freedom_tower),
-        (Point(2, 3), Point(6, 4)),
-        (Point(6, 2), Point(9, 2)),
+        (empire_state_building, willis_tower),
     ], ["start", "end"])
 
     res = df.withColumn(
-        "st_distance", 
+        "st_distance_sphere", 
         ST_DistanceSphere(col("start"), col("end"))
     ).withColumn(
         "start_cellid", 
@@ -31,9 +31,9 @@ def test_H3CellDistance():
         "end_cellid", 
         ST_H3CellIDs(col("end"), 6, True)
     ).withColumn(
-        "h3_distance", 
+        "h3_cell_distance", 
         ST_H3CellDistance(col("start_cellid")[0], col("end_cellid")[0])
     )
 
     print("***")
-    res.select("st_distance", "h3_distance").show()
+    res.select("st_distance_sphere", "h3_cell_distance").show()
